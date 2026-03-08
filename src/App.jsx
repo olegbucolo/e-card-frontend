d5ce77f48c2e9963165e80c8da10b4a75509e96c
olegbucolo <olegbucolo97@gmail.com>
2026-03-08 17:04:59 +0100

    Aggiunto header base

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import DefaultLayout from "./layouts/DefaultLayout";
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
