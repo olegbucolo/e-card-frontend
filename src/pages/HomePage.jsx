361f35b6f35ef6713cbdbcdc0453c122d199682c
FrancescoCaporale <frenk.caporale@gmail.com>
2026-03-08 17:26:34 +0100

    add map in featured products

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,5 @@
+import { products } from "../data/products";
+
 export default function HomePage() {
     return (
         <>
@@ -13,31 +15,47 @@ export default function HomePage() {
                     </div>
                 </div>
             </section>
+
             <section className="categories">
                 <div className="container">
                     <h2>Esplora per categoria</h2>
+
                     <div className="categories-grid">
-                        <div className="category-card">Pokemon</div>
+                        <div className="category-card">Pokémon</div>
                         <div className="category-card">Yu-Gi-Oh!</div>
                         <div className="category-card">Magic</div>
                         <div className="category-card">One Piece</div>
                     </div>
                 </div>
             </section>
+
             <section className="featured-products">
                 <div className="container">
                     <h2>Prodotti in evidenza</h2>
+
                     <div className="products-grid">
-                        <div className="product-card">Prodotto 1</div>
-                        <div className="product-card">Prodotto 2</div>
-                        <div className="product-card">Prodotto 3</div>
-                        <div className="product-card">Prodotto 4</div>
+                        {products.map((product) => (
+                            <div className="product-card" key={product.product_id}>
+                                <div className="product-image">
+                                    <span>{product.image}</span>
+                                </div>
+
+                                <div className="product-info">
+                                    <h3>{product.name}</h3>
+                                    <p>{product.description}</p>
+                                    <span className="product-price">€ {product.price}</span>
+                                    <button>Aggiungi al carrello</button>
+                                </div>
+                            </div>
+                        ))}
                     </div>
                 </div>
             </section>
+
             <section className="shop-benefits">
                 <div className="container">
                     <h2>Perché scegliere noi</h2>
+
                     <div className="benefits-grid">
                         <div className="benefit-card">Spedizione veloce</div>
                         <div className="benefit-card">Carte selezionate</div>
@@ -46,5 +64,5 @@ export default function HomePage() {
                 </div>
             </section>
         </>
-    )
+    );
 }
