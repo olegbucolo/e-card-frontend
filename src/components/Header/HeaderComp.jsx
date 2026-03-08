8c136a2c1f67ef9ce96737407a81fdfde3c0d490
olegbucolo <olegbucolo97@gmail.com>
2026-03-08 17:10:23 +0100

    fix: class to className in header and NavLink

--- src/components/Header/HeaderComp.jsx
+++ src/components/Header/HeaderComp.jsx
@@ -1,27 +1,27 @@
+import { NavLink } from "react-router-dom";
+import HomePage from "../../pages/HomePage";
+
 export default function HeaderComp() {
     return (
         <>
             <header>
-                <nav class="navbar navbar-expand-lg bg-body-tertiary">
-                    <div class="container-fluid">
-                        <a class="navbar-brand" href="#"><span className="text-warning">e</span>-card</a>
-                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon"></span>
+                <nav className="navbar navbar-expand-lg bg-body-tertiary">
+                    <div className="container-fluid">
+                        <a className="navbar-brand" href="#"><span className="text-warning">e</span>-card</a>
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
                         </button>
-                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                                <li class="nav-item">
-                                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                                <li className="nav-item">
+                                    <NavLink to="/" className="nav-link active" aria-current="page" >Home</NavLink>
                                 </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">Shop</a>
+                                <li className="nav-item">
+                                    <a href="#" className="nav-link" >Shop</a>
                                 </li>
                                 
                             </ul>
-                            <form class="d-flex" role="search">
-                                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                                <button class="btn btn-outline-success" type="submit">Search</button>
-                            </form>
+                            
                         </div>
                     </div>
                 </nav>
