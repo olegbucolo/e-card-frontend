28357b082c91c796aa0bff99b7226543b76c7ad5
olegbucolo <olegbucolo97@gmail.com>
2026-03-09 17:03:34 +0100

    refactor: remove class=

--- src/pages/ShopPage.jsx
+++ src/pages/ShopPage.jsx
@@ -4,13 +4,13 @@ import { products } from '../data/products'
 export default function ShopPage() {
     return (
         <>
-            <div className="container-lg mt-2">
+            <div className="container-lg my-3">
                 <div className="col">
                     <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-5 g-2">
                         {products.map(p => (
                             <div className="col">
                                 <div className="card hover-card h-100 p-2">
-                                    <img src={drago} class=" card-img-top" alt="..." />
+                                    <img src={drago} className="card-img-top" alt="..." />
                                     <div className="card-body px-0">
                                         <h5 className="card-title">{p.name}</h5>
                                         <p className="card-text">{p.description}</p>
