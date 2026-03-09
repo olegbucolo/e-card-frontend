4a0cd1c5b44d69fa58fb26276622913ee9570a16
olegbucolo <olegbucolo97@gmail.com>
2026-03-09 12:53:00 +0100

    add column to manage gap

--- src/pages/ShopPage.jsx
+++ src/pages/ShopPage.jsx
@@ -8,11 +8,13 @@ export default function ShopPage() {
                 <div class="col">
                     <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-5 g-4">
                         {products.map(p => (
-                            <div class="card">
-                                <img src={drago} class=" card-img-top" alt="..." />
-                                <div class="card-body">
-                                    <h5 class="card-title">{p.name}</h5>
-                                    <p class="card-text">{p.description}</p>
+                            <div className="col">
+                                <div class="card">
+                                    <img src={drago} class=" card-img-top" alt="..." />
+                                    <div class="card-body">
+                                        <h5 class="card-title">{p.name}</h5>
+                                        <p class="card-text">{p.description}</p>
+                                    </div>
                                 </div>
                             </div>
                         ))}
