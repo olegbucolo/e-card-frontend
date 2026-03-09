4f928057d6400bfadbc7b96c048149aeddb1f8ae
Pietro Scurti <pietroscurti9@gmail.com>
2026-03-09 16:41:13 +0100

    fix className

--- src/components/Toast.jsx
+++ src/components/Toast.jsx
@@ -4,9 +4,9 @@ export default function Toast() {
 
             <div className="toast" role="alert" aria-live="assertive" aria-atomic="true">
                 <div className="toast-header">
-                    {/* <img src="..." className="rounded me-2" alt="..."> */}
+                
                     <strong className="me-auto">e-card</strong>
-                    {/* <small>11 mins ago</small> */}
+                    
                     <button type="button" className="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
                 </div>
                 <div className="toast-body">
