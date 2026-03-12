69ba38d6571474fb6ef66831ac8a48b38ef444b0
olegbucolo <olegbucolo97@gmail.com>
2026-03-12 02:10:46 +0100

    add axios call index products in DefaultLayout comp

--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -1,16 +1,35 @@
 import { Outlet } from "react-router-dom";
 import HeaderComp from "../components/Header/HeaderComp";
 import FooterComp from "../components/Footer/FooterComp";
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
-
-const indexProductsAPI = 'http://localhost:3000/products'
+const indexProductsAPI = 'http://localhost:3000/products/'
 
 export default function DefaultLayout() {
+
+    const [indexProducts, setIndexProducts] = useState([])
+
+    useEffect(() => {
+        async function fetchProducts() {
+            try {
+                const res = await axios.get(indexProductsAPI)
+                setIndexProducts(res.data);
+                console.log(res.data)
+
+            } catch (err) {
+                console.error(err)
+            }
+        }
+        fetchProducts();
+
+    }, [])
+
     return (
         <>
             <HeaderComp />
             <main>
-                <Outlet />
+                <Outlet products={indexProducts} />
             </main>
             <FooterComp />
         </>
