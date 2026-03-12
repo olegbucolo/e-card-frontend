5147e36f5b88ff02078dafd7fbf68e0f814f091e
olegbucolo <olegbucolo97@gmail.com>
2026-03-12 01:40:41 +0100

    imports

--- src/pages/ShopPage.jsx
+++ src/pages/ShopPage.jsx
@@ -7,41 +7,40 @@ export default function ShopPage() {
     const [searchParams] = useSearchParams();
     const search = searchParams.get("search")
     const filteredProducts = products.filter(p => p.name.toLowerCase().includes(search?.toLowerCase() || ""))
-    console.log(filteredProducts)
     return (
         <>
             <div className="container-lg my-5 pt-3">
                 <div className="col">
                     <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-5 g-2">
                         {search ? filteredProducts.map(p => {
-                                return (
-                                    <div key={p.product_id} className="col">
+                            return (
+                                <div key={p.product_id} className="col">
 
-                                        <div className="card hover-card h-100 p-2 d-flex flex-column">
+                                    <div className="card hover-card h-100 p-2 d-flex flex-column">
 
-                                            <NavLink to={`/detailpage/${p.product_id}`} className="text-decoration-none text-dark">
-                                                <img src={drago} className="card-img-top" alt="" />
+                                        <NavLink to={`/detailpage/${p.product_id}`} className="text-decoration-none text-dark">
+                                            <img src={drago} className="card-img-top" alt="" />
 
-                                                <div className="card-body px-0">
-                                                    <h5 className="card-title fs-5">{p.name}</h5>
-                                                    <p className="card-text ">{p.description}</p>
-                                                </div>
-                                            </NavLink>
-
-                                            <div className="buttons d-flex justify-content-between mt-auto">
-                                                <button className="hover-button btn btn-success w-50 me-2">
-                                                    Add to Cart
-                                                </button>
-                                                <button className="hover-button btn btn-warning w-50">
-                                                    Wishlist
-                                                </button>
+                                            <div className="card-body px-0">
+                                                <h5 className="card-title fs-5">{p.name}</h5>
+                                                <p className="card-text ">{p.description}</p>
                                             </div>
+                                        </NavLink>
 
+                                        <div className="buttons d-flex justify-content-between mt-auto">
+                                            <button className="hover-button btn btn-success w-50 me-2">
+                                                Add to Cart
+                                            </button>
+                                            <button className="hover-button btn btn-warning w-50">
+                                                Wishlist
+                                            </button>
                                         </div>
 
                                     </div>
-                                )
-                            }) : <div className='w-100'>You haven't searched anything yet</div>}
+
+                                </div>
+                            )
+                        }) : <div className='w-100'>You haven't searched anything yet</div>}
                     </div>
                 </div>
             </div>
