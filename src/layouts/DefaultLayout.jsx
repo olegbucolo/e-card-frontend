553ffa9feb21a89597a85ea69b5f4b46b6963a57
olegbucolo <olegbucolo97@gmail.com>
2026-03-12 01:53:06 +0100

    Adding OutletContext

--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -2,6 +2,9 @@ import { Outlet } from "react-router-dom";
 import HeaderComp from "../components/Header/HeaderComp";
 import FooterComp from "../components/Footer/FooterComp";
 
+
+const indexProductsAPI = 'http://localhost:3000/products'
+
 export default function DefaultLayout() {
     return (
         <>
